d6c892910c7001c1b94a48b454d3f8927789162c
Matthew <mgball@uci.edu>
2026-02-15 14:32:57 -0800

    made adjustments to add python lsp and custom apps to open google-calendar and books folder app icons

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,35 +8,68 @@ return {
     },
     config = function()
       require("mason").setup()
+      local mason_lspconfig = require("mason-lspconfig")
+      local lspconfig = require("lspconfig")
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-      -- mason-lspconfig v2+ can auto-enable servers you install
-      require("mason-lspconfig").setup({
+      -- Advertise capabilities for nvim-cmp
+      local capabilities = cmp_nvim_lsp.default_capabilities()
+
+      mason_lspconfig.setup({
         ensure_installed = {
           "lua_ls",
           "pyright",
+          "ruff", -- Python linting/formatting
           "html",
           "jdtls",
         },
-        -- automatic_enable = true, -- (this is the default)
-      }) -- :contentReference[oaicite:1]{index=1}
+        handlers = {
+          -- Default handler for installed servers
+          function(server_name)
+            lspconfig[server_name].setup({
+              capabilities = capabilities,
+            })
+          end,
 
-      -- Configure servers with the *new* API (no require("lspconfig")!)
-      vim.lsp.config("lua_ls", {
-        settings = {
-          Lua = {
-            diagnostics = { globals = { "vim" } },
-          },
+          -- Targeted overrides for specific servers
+          ["lua_ls"] = function()
+            lspconfig.lua_ls.setup({
+              capabilities = capabilities,
+              settings = {
+                Lua = {
+                  diagnostics = { globals = { "vim" } },
+                },
+              },
+            })
+          end,
+
+          ["pyright"] = function()
+             lspconfig.pyright.setup({
+              capabilities = capabilities,
+               settings = {
+                 python = {
+                   analysis = {
+                     typeCheckingMode = "basic", -- or "strict"
+                     autoSearchPaths = true,
+                     useLibraryCodeForTypes = true,
+                   }
+                 }
+               }
+             })
+          end,
+
+          -- Ruff config (mostly default is good for now)
+          ["ruff"] = function()
+               lspconfig.ruff.setup({
+                  -- Disable hover in favor of Pyright
+                  on_attach = function(client, _)
+                    client.server_capabilities.hoverProvider = false
+                  end
+               })
+          end,
         },
-      }) -- :contentReference[oaicite:2]{index=2}
+      })
 
-      vim.lsp.config("pyright", {})
-      vim.lsp.config("html", {})
-      vim.lsp.config("metals", {})
-      vim.lsp.config("jdtls", {})
-
-      -- If you turned off mason-lspconfig auto-enable, enable explicitly:
-      -- vim.lsp.enable({ "lua_ls", "pyright", "html", "metals", "jdtls" })
-      -- :contentReference[oaicite:3]{index=3}
     end,
   },
 }
--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -3,3 +3,23 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", function()
   vim.cmd("Neotree toggle")
 end)
+
+-- Run current file
+vim.keymap.set("n", "<leader>r", function()
+  local filetype = vim.bo.filetype
+  if filetype == "python" then
+    vim.cmd("write")
+    vim.cmd("!python3 %")
+  elseif filetype == "lua" then
+    vim.cmd("write")
+    vim.cmd("source %")
+  else
+    print("No runner for filetype: " .. filetype)
+  end
+end)
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
