ac43383389deaeae457910ff0566d32b98cbc0b2
Matthew Ball <mgball@uci.edu>
2026-06-04 20:37:14 -0700

    changed the shortcut to be mr for rendering markdown

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -9,7 +9,7 @@ return {
   config = function(_, opts)
     require('render-markdown').setup(opts)
     -- Toggle rendering on/off
-    vim.keymap.set('n', '<leader>tm', '<cmd>RenderMarkdown toggle<cr>',
-      { desc = 'Toggle markdown rendering' })
+    vim.keymap.set('n', '<leader>mr', '<cmd>RenderMarkdown toggle<cr>',
+      { desc = 'Markdown: toggle render' })
   end,
 }
